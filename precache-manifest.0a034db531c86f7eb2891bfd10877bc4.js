141a8dc7d452460ba10a1a5d0f33fa20b07c0f4c
shankS3 <shashank.surwase@gmail.com>
2020-10-12 03:57:28 +0530

    Updates

---
+++ precache-manifest.0a034db531c86f7eb2891bfd10877bc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59d8616fc5949ea45adb102d61b7f0e",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "e919eb4eb7f670aae333",
+    "url": "/hackernews/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1faa2d40c87cbdef0869",
+    "url": "/hackernews/static/css/main.82113c0c.chunk.css"
+  },
+  {
+    "revision": "e919eb4eb7f670aae333",
+    "url": "/hackernews/static/js/2.5ebb73a0.chunk.js"
+  },
+  {
+    "revision": "3043e3e727d80f353557f855ab1ce92b",
+    "url": "/hackernews/static/js/2.5ebb73a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1faa2d40c87cbdef0869",
+    "url": "/hackernews/static/js/main.4923a1a0.chunk.js"
+  },
+  {
+    "revision": "b6ce03fa6b59141832ff",
+    "url": "/hackernews/static/js/runtime-main.fe423155.js"
+  }
+]);
--- src/containers/PostDetailsContainer.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux';
-import Posts from '../components/PostDetails';
-
-const PostDetailsContainer = connect(null, null)(Posts);
-
-export default PostDetailsContainer;
